e015a6b83b6122bd86804ebb987b4373302d228c
Heinkel66 <Heinkel.66@gmail.com>
2024-10-21 10:58:06 -0500

    PC 21-10-2024 - Primer commit

---
+++ app/src/main/java/com/aquarius/indra_challenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aquarius.indra_challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
